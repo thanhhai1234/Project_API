a9d6472a99eb8ea46126d0e07083129155b7748b
thanhhai1234 <87158065+thanhhai1234@users.noreply.github.com>
2023-11-23 17:25:46 +0700

    delete

--- restapi-golang/restapi-golang/models/User.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	ID       uint   `json:"id" gorm:"primary_key"`
-	Name     string `json:"name"`
-	Password string `json:"password" binding:"required"`
-	Tasks    []Task `gorm:"foreignkey:UserID"`
-}
